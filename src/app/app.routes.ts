ade9f96260dcf7f7eb53428944e8931ba4a11600
imHinotori <hello@hinotori.moe>
2023-09-13 19:51:09 -0300

    Removed Tab Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
     path: '',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
   {
     path: 'student-classes',
     loadComponent: () => import('./student-classes/student-classes.page').then( m => m.StudentClassesPage)
-  },  {
+  },
+  {
     path: 'student-class',
     loadComponent: () => import('./student-class/student-class.page').then( m => m.StudentClassPage)
   },
