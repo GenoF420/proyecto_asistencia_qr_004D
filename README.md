fd3356e82a994938d7f96f6c4d618bdf1a3cb311
imHinotori <hello@hinotori.moe>
2023-10-13 19:12:01 -0300

    Base Project with NGModules

--- README.md
+++
@@ -1,3 +0,0 @@
-# proyecto_asistencia_qr_004D
-Esta es una evaluación que corresponde a una entrega de encargo con presentación y tiene un 40% de ponderación sobre la nota final de la asignatura.
-A
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Asistencia',
+  appName: 'QRDuoc',
   webDir: 'www',
   server: {
     androidScheme: 'https'
