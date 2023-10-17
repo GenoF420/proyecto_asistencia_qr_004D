f8067340e289a5dc0476568468d5ff250c8482fd
pate9596 <106767172+DeelanMaldonado@users.noreply.github.com>
2023-10-16 21:21:33 -0300

    Se agrego metodos de login y delete y se arreglo el error anterior

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, public toastController: ToastController, private api:ApiService) { }
 
-  ngOnInit() {
-  }
 
   login() {
     const usernameInput = document.getElementById('username') as HTMLIonInputElement;
     const passwordInput = document.getElementById('password') as HTMLIonInputElement;
+
+    let username = new String(usernameInput.value);
+    let password = new String(passwordInput.value);
+
+    
+
+    if (this.api.login(username,password)) {
+      console.log("Es valido")
+      // La cuenta es válida, puedes redirigir al usuario según el tipo de cuenta (account.type)
+    }else{
+      console.log("no es valida");
+    }   
+  }
+
+  goToPage(page: string) {
+    this.router.navigate([page])
+  }
+
+  createToast(title: string, msg: string, pos: any, dur: number) {
+    this.toastController.create({
+      header: title,
+      message: msg,
+      position: pos,
+      duration: dur,
+      cssClass: "custom-toast"
+    }).then((toast) => {
+      toast.present();
+    });
+  }
+
+  ngOnInit() {
   }
 
 }
